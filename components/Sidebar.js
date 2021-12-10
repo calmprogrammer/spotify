8af19bf2fc272f4d1a13cdc7fa066b7083d85377
Jarreau Fourie <jarreaufourie@outlook.com>
2021-12-10 18:46:32 +0200

    user playlists added to sidebar

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -17,13 +17,13 @@ function Sidebar() {
 
 	useEffect(() => {
 		if (spotifyApi.getAccessToken()) {
-			spotifyApi.getUserPlaylists.then((data) => {
+			spotifyApi.getUserPlaylists().then((data) => {
 				setPlaylists(data.body.items);
 			});
 		}
 	}, [session, spotifyApi]);
 
-	console.log(session);
+	console.log(playlists);
 	return (
 		<div className="text-gray-500 p-5 text-sm border-r border-gray-900 overflow-y-scroll scrollbar-hide h-screen">
 			<div className="space-y-4">
@@ -62,16 +62,11 @@ function Sidebar() {
 				<hr className="border-t-[0.1px] border-gray-900" />
 
 				{/* Playlist */}
-				<p className="cursor-pointer hover:text-white">Playlist name...</p>
-				<p className="cursor-pointer hover:text-white">Playlist name...</p>
-				<p className="cursor-pointer hover:text-white">Playlist name...</p>
-				<p className="cursor-pointer hover:text-white">Playlist name...</p>
-				<p className="cursor-pointer hover:text-white">Playlist name...</p>
-				<p className="cursor-pointer hover:text-white">Playlist name...</p>
-				<p className="cursor-pointer hover:text-white">Playlist name...</p>
-				<p className="cursor-pointer hover:text-white">Playlist name...</p>
-				<p className="cursor-pointer hover:text-white">Playlist name...</p>
-				<p className="cursor-pointer hover:text-white">Playlist name...</p>
+				{playlists.map((playlist) => (
+					<p key={playlist.id} className="cursor-pointer hover:text-white">
+						{playlist.name}
+					</p>
+				))}
 			</div>
 		</div>
 	);
