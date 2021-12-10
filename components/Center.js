769c6b335430958d53a47397db5b2c543b37c606
Jarreau Fourie <jarreaufourie@outlook.com>
2021-12-10 18:07:53 +0200

    added gradiend background to center section

--- components/Center.js
+++ components/Center.js
@@ -1,10 +1,27 @@
 import { ChevronDownIcon } from "@heroicons/react/outline";
 import { useSession } from "next-auth/react";
+import { useEffect, useState } from "react";
+import { shuffle } from "lodash";
+
+const colors = [
+	"from-indigo-500",
+	"from-blue-500",
+	"from-green-500",
+	"from-red-500",
+	"from-yellow-500",
+	"from-pink-500",
+	"from-purple-500",
+];
 
 function Center() {
 	const { data: session } = useSession();
+	const [color, setColor] = useState(null);
+	useEffect(() => {
+		setColor(shuffle(colors).pop());
+	});
+
 	return (
-		<div className="flex flex-grow text-white">
+		<div className="flex-grow text-white">
 			<header className="absolute top-5 right-8">
 				<div className="flex items-center bg-black space-x-3 opacity-90 hover:opacity-80 cursor-pointer rounded-full p-1 pr-2">
 					<img
@@ -18,7 +35,7 @@ function Center() {
 			</header>
 
 			<section
-				className={`flex items-end space-x-7 bg-gradient-to-b to-black from-red-500 h-80 text-white`}
+				className={`flex items-end space-x-7 bg-gradient-to-b to-black ${color} h-80 text-white `}
 			>
 				<h1>hello</h1>
 			</section>
