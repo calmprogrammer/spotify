52d59eb88eb6e393bb406fa2af87c7465c7f2a6b
Jarreau Fourie <jarreaufourie@outlook.com>
2021-12-10 19:17:36 +0200

    display playlist on center component

--- components/Center.js
+++ components/Center.js
@@ -55,7 +55,16 @@ function Center() {
 			<section
 				className={`flex items-end space-x-7 bg-gradient-to-b to-black ${color} h-80 text-white `}
 			>
-				<h1>hello</h1>
+				<img
+					className="h-44 w-44 shadow-2xl"
+					src={playlist?.images?.[0]?.url}
+				/>
+				<div>
+					<p>PLAYLIST</p>
+					<h1 className="text-2xl md:text-3xl xl:text-5xl font-bold">
+						{playlist?.name}
+					</h1>
+				</div>
 			</section>
 		</div>
 	);
