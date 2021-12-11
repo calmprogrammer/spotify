9f05c80a1f618f75a480f5de35e2ff4741ad48f0
Jarreau Fourie <jarreaufourie@outlook.com>
2021-12-11 07:34:24 +0200

    volume adjustable and play pause enabled

--- components/Player.js
+++ components/Player.js
@@ -1,9 +1,20 @@
 import { useSession } from "next-auth/react";
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { useRecoilState } from "recoil";
 import { currentTrackIdState, isPlayingState } from "../atoms/songAtom";
 import useSongInfo from "../hooks/useSongInfo";
 import useSpotify from "../hooks/useSpotify";
+import {
+	FastForwardIcon,
+	PauseIcon,
+	PlayIcon,
+	ReplyIcon,
+	RewindIcon,
+	SwitchHorizontalIcon,
+	VolumeOffIcon,
+	VolumeUpIcon,
+} from "@heroicons/react/solid";
+import { debounce } from "lodash";
 
 function Player() {
 	const spotifyApi = useSpotify();
@@ -18,6 +29,7 @@ function Player() {
 	const fetchCurrentSong = () => {
 		if (!songInfo) {
 			spotifyApi.getMyCurrentPlayingTrack().then((data) => {
+				console.log(`Now playing : ${data.body?.item}`);
 				setCurrentIdTrack(data.body?.item?.item);
 
 				spotifyApi.getMyCurrentPlaybackState().then((data) => {
@@ -27,6 +39,18 @@ function Player() {
 		}
 	};
 
+	const handlePlayPause = () => {
+		spotifyApi.getMyCurrentPlaybackState().then((data) => {
+			if (data.body?.is_playing) {
+				spotifyApi.pause();
+				setIsPlaying(false);
+			} else {
+				spotifyApi.play();
+				setIsPlaying(true);
+			}
+		});
+	};
+
 	useEffect(() => {
 		if (spotifyApi.getAccessToken() && !currentTrackId) {
 			fetchCurrentSong();
@@ -34,18 +58,63 @@ function Player() {
 		}
 	}, [currentTrackIdState, spotifyApi, session]);
 
+	useEffect(() => {
+		if (volume > 0 && volume < 100) {
+			debounceAdjustVolume(volume);
+		}
+	}, [volume]);
+
+	const debounceAdjustVolume = useCallback(
+		debounce((volume) => {
+			spotifyApi.setVolume(volume).catch((err) => {});
+		}, 500),
+		[]
+	);
+
 	return (
 		<div className="h-24 bg-gradient-to-b from-black to-gray-900 text-white grid grid-cols-3 text-xs md:text-base px-2 md:px-8">
 			<div className="flex items-center space-x-4">
 				<img
 					className="hidden md:inline h10 w-10"
-					src={songInfo?.album.images?.[0].url}
+					src={songInfo?.album.images?.[0]?.url}
 					alt=""
 				/>
 				<div>
 					<h3>{songInfo?.name}</h3>
 					<p>{songInfo?.artists?.[0]?.name}</p>
 				</div>
+			</div>
+
+			<div className="flex items-center justify-evenly">
+				<SwitchHorizontalIcon className="button" />
+				<RewindIcon className="button" />
+
+				{isPlaying ? (
+					<PauseIcon onClick={handlePlayPause} className="button w-10 h-10" />
+				) : (
+					<PlayIcon onClick={handlePlayPause} className="button w-10 h-10" />
+				)}
+				<FastForwardIcon className="button" />
+				<ReplyIcon className="button" />
+			</div>
+
+			<div className="flex items-center space-x-3 md:space-x-4 justify-end pr-5">
+				<VolumeOffIcon
+					onClick={() => volume > 0 && setVolume(volume - 10)}
+					className="button"
+				/>
+				<input
+					className="w-14 md:w-28"
+					type="range"
+					value={volume}
+					onChange={(e) => setVolume(Number(e.target.value))}
+					min={0}
+					max={100}
+				/>
+				<VolumeUpIcon
+					className="button"
+					onClick={() => volume < 100 && setVolume(volume + 10)}
+				/>
 			</div>
 		</div>
 	);
